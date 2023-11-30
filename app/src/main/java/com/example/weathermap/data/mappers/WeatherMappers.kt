3eade8449c4eb3183fe4292dbd8b15517c3f95d8
Anton Gorokhov <bonduelle85@gmail.com>
2023-11-30 21:03:19 +0500

    2 mappers

---
+++ app/src/main/java/com/example/weathermap/data/mappers/WeatherMappers.kt
@@ -0,0 +1,56 @@
+package com.example.weathermap.data.mappers
+
+import com.example.weathermap.data.remote.WeatherDataDto
+import com.example.weathermap.data.remote.WeatherDto
+import com.example.weathermap.domain.weather.WeatherData
+import com.example.weathermap.domain.weather.WeatherInfo
+import com.example.weathermap.domain.weather.WeatherType
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+// The Map does not contain indexes, so we create a private data class IndexedWeatherData.
+private data class IndexedWeatherData(
+    val index: Int,
+    val weatherData: WeatherData
+)
+
+fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
+    return time.mapIndexed { index: Int, time: String ->
+        val temperature = temperatures[index]
+        val pressure = pressures[index]
+        val windSpeed = windSpeeds[index]
+        val humidity = humidities[index]
+        val weatherCode = weatherCodes[index]
+        IndexedWeatherData(
+            index = index,
+            weatherData = WeatherData(
+                time = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME),
+                temperatureCelsius = temperature,
+                pressure = pressure,
+                windSpeed = windSpeed,
+                humidity = humidity,
+                weatherType = WeatherType.fromWMO(weatherCode)
+            )
+        )
+    }.groupBy { indexedWeatherData ->
+        // Each "index/24" (24 indexes per day in Json) will correspond to a day: 0 - today, 1 - tomorrow, etc.
+        indexedWeatherData.index / 24
+    }.mapValues { entry ->
+        entry.value.map { indexedWeatherData ->
+            indexedWeatherData.weatherData
+        }
+    }
+}
+
+fun WeatherDto.toWeatherInfo(): WeatherInfo {
+    val weatherDataMap = weatherData.toWeatherDataMap()
+    val currentTime = LocalDateTime.now()
+    val currentWeatherData = weatherDataMap[0]?.find { weatherData ->
+        val hour = if (currentTime.minute > 30) currentTime.hour else currentTime.hour + 1
+        weatherData.time.hour==hour
+    }
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeatherData
+    )
+}
